d40c80065a21ba6ca333ff180609c97c6cc83431
Hank Breckenridge <breckenridgeh2@gmail.com>
2025-08-25 14:24:19 -0600

    Swap version back to 3.1.0

--- src/main/java/com/snowflake/kafka/connector/Utils.java
+++ src/main/java/com/snowflake/kafka/connector/Utils.java
@@ -66,7 +66,7 @@ import org.apache.kafka.common.config.ConfigValue;
 public class Utils {
 
   // Connector version, change every release
-  public static final String VERSION = "3.2.0";
+  public static final String VERSION = "3.1.0";
 
   // connector parameter list
   public static final String NAME = "name";
